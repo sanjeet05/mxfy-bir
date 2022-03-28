616f7df1338da44a38f7745925e9de8354bd37a0
sanjeet <sanjeet.kumar@twinhealth.com>
2022-03-28 22:36:29 +0530

    added table

--- js/bir.js
+++ js/bir.js
@@ -5,3 +5,29 @@ function toggleLeftSidebar(event) {
 		templeteLayout[0].classList.toggle("templete_layout_collapse");
 	}
 }
+
+// dashboard
+function dashboardInit() {
+	document.getElementById("tab_1").classList.toggle("db_tab_heading_active");
+	document.getElementById("tab_1_data").classList.toggle("d-block");
+
+	document.getElementById("tab_2_data").classList.toggle("d-none");
+	document.getElementById("tab_3_data").classList.toggle("d-none");
+	document.getElementById("tab_4_data").classList.toggle("d-none");
+}
+
+function activeDashboardTab(tabNumber) {
+	if (tabNumber === 1) {
+		document.getElementById("tab_1").classList.toggle("db_tab_heading_active");
+		document.getElementById("tab_1_data").classList.toggle("d-none");
+	} else if (tabNumber === 2) {
+		document.getElementById("tab_2").classList.toggle("db_tab_heading_active");
+		document.getElementById("tab_2_data").classList.toggle("d-none");
+	} else if (tabNumber === 3) {
+		document.getElementById("tab_3").classList.toggle("db_tab_heading_active");
+		document.getElementById("tab_3_data").classList.toggle("d-none");
+	} else if (tabNumber === 4) {
+		document.getElementById("tab_4").classList.toggle("db_tab_heading_active");
+		document.getElementById("tab_4_data").classList.toggle("d-none");
+	}
+}
