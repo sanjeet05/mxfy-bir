ae6487f48a16462ae8c7d94e3846326e8b02351c
sanjeet <sanjeet.kumar@twinhealth.com>
2022-05-31 16:13:16 +0530

    config upload issue fixed

--- js/bir.js
+++ js/bir.js
@@ -1,3 +1,42 @@
+// common js - start
+// for custom select options
+for (const dropdown of document.querySelectorAll(".select_wrapper")) {
+	dropdown.addEventListener("click", function () {
+		this.querySelector(".select_container").classList.toggle("open");
+	});
+}
+
+for (const option of document.querySelectorAll(".select_custom_option")) {
+	option.addEventListener("click", function () {
+		if (!this.classList.contains("selected")) {
+			const selectedEle = this.parentNode.querySelector(
+				".select_custom_option.selected"
+			);
+			if (selectedEle) {
+				selectedEle.classList.remove("selected");
+			}
+			this.classList.add("selected");
+
+			this.closest(".select_container")
+				.querySelector(".select__trigger")
+				.classList.add("selected");
+			this.closest(".select_container").querySelector(
+				".select__trigger span"
+			).textContent = this.textContent;
+		}
+	});
+}
+
+document.addEventListener("click", function (e) {
+	for (const select of document.querySelectorAll(".select_container")) {
+		if (!select.contains(e.target)) {
+			select.classList.remove("open");
+		}
+	}
+});
+
+// common js - end
+
 // on click of left panel close double arrow
 function toggleLeftSidebar(event) {
 	const templeteLayout = document.querySelectorAll(".templete_layout");
@@ -51,9 +90,21 @@ function dashboardInit() {
 	document.getElementById("tab_1").classList.toggle("db_tab_heading_active");
 	document.getElementById("tab_1_data").classList.toggle("d-block");
 
-	document.getElementById("tab_2_data").classList.toggle("d-none");
-	document.getElementById("tab_3_data").classList.toggle("d-none");
-	document.getElementById("tab_4_data").classList.toggle("d-none");
+	document.getElementById("tab_2_data")?.classList.toggle("d-none");
+	document.getElementById("tab_3_data")?.classList.toggle("d-none");
+	document.getElementById("tab_4_data")?.classList.toggle("d-none");
+
+	document
+		.getElementById("modal_tab_1")
+		.classList.toggle("db_tab_heading_active");
+	document.getElementById("modal_tab_1_data").classList.toggle("d-block");
+
+	document.getElementById("modal_tab_2_data").classList.toggle("d-none");
+	document.getElementById("modal_tab_3_data").classList.toggle("d-none");
+	document.getElementById("modal_tab_4_data").classList.toggle("d-none");
+	document.getElementById("modal_tab_5_data").classList.toggle("d-none");
+	document.getElementById("modal_tab_6_data").classList.toggle("d-none");
+	document.getElementById("modal_tab_7_data").classList.toggle("d-none");
 }
 
 function activeDashboardTab(tabNumber) {
@@ -99,4 +150,43 @@ function handleModalSearch() {
 	document
 		.getElementById("createReport")
 		.classList.remove("modal_save_btn_dis");
+}
+
+function activeModalDashboardTab(tabNumber) {
+	if (tabNumber === 1) {
+		document
+			.getElementById("modal_tab_1")
+			.classList.toggle("db_tab_heading_active");
+		document.getElementById("modal_tab_1_data").classList.toggle("d-none");
+	} else if (tabNumber === 2) {
+		document
+			.getElementById("modal_tab_2")
+			.classList.toggle("db_tab_heading_active");
+		document.getElementById("modal_tab_2_data").classList.toggle("d-none");
+	} else if (tabNumber === 3) {
+		document
+			.getElementById("modal_tab_3")
+			.classList.toggle("db_tab_heading_active");
+		document.getElementById("modal_tab_3_data").classList.toggle("d-none");
+	} else if (tabNumber === 4) {
+		document
+			.getElementById("modal_tab_4")
+			.classList.toggle("db_tab_heading_active");
+		document.getElementById("modal_tab_4_data").classList.toggle("d-none");
+	} else if (tabNumber === 5) {
+		document
+			.getElementById("modal_tab_5")
+			.classList.toggle("db_tab_heading_active");
+		document.getElementById("modal_tab_5_data").classList.toggle("d-none");
+	} else if (tabNumber === 6) {
+		document
+			.getElementById("modal_tab_6")
+			.classList.toggle("db_tab_heading_active");
+		document.getElementById("modal_tab_6_data").classList.toggle("d-none");
+	} else if (tabNumber === 7) {
+		document
+			.getElementById("modal_tab_7")
+			.classList.toggle("db_tab_heading_active");
+		document.getElementById("modal_tab_7_data").classList.toggle("d-none");
+	}
 }
